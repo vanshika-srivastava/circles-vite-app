ff5973a70a04788c90dbf48ea9890f02b7becbb6
Vanshika Srivastava <vanshikasrivastava.business@gmail.com>
2025-02-12 18:10:32 +0530

    Update App.jsx

--- src/circles-components/App.jsx
+++ src/circles-components/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Dashboard from "./dashboard";
-import CirclesOnboarding from "./circlesOnboarding";
+import CirclesOnboarding from "./circlesonboarding";
 import { CirclesSDK } from "../contexts/CirclesSDK";
 
 export default function App() {
